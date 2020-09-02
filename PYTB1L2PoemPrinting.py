278cc23ca73a270174ba5e0bad1d2abd571ecf83
Martijn van der Meer <martijnrenevdmeer@gmail.com>
2020-09-02 13:51:41 +0200

    Poem added

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,9 @@
+vers1 = "Mijn naam is Martijn van der Meer, ik loop vaak heen en weer. "
+vers2 = "Ik woon nog bij mijn moeder thuis, daar hang ik best vaak voor de buis. "
+vers3 = "Ik wil goed leren coderen, en hou van nieuwe dingen leren. "
+vers4 = "Ik vind het leuk om games te spelen, maar dat brengt ook weer zijn nadelen. "
+vers5 = "Hardlopen dat lukt ook goed, maar heb soms een zere voet. "
+vers6 = "Dit gedicht duurt niet al te lang, want van te veel woorden wordt ik bang. "
+vers7 = "Tel nu maar van 1 tot 10, want ik hou het nu voor gezien!"
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7)
