9f59cc86ffc4e6d41c1c5468169f6a8908b07dc0
Martijn van der Meer <martijnrenevdmeer@gmail.com>
2020-10-07 11:41:22 +0200

    Variabelen en functions ge-defined

--- B1L3/PYTB1L3DeeplyNested.py
+++ B1L3/PYTB1L3DeeplyNested.py
@@ -1,7 +1,21 @@
+hungry = True
+cookmood = False
+leftovers = False
+money = 210
+
+def goCook():
+	print("Ga koken!")
+def orderPizza():
+	print("Bestel een pizza.")
+def eatLeftovers():
+	print("Eet nog wat restjes van gisteren.")
+def eatNothing():
+	print("Als je geen honger hebt hoef je ook niet te eten.")
+
 if hungry:
-	if !cookmood:
-		if !leftovers:
-			if !money:
+	if not cookmood:
+		if not leftovers:
+			if money < 200:
 				goCook()
 			else:
 				orderPizza()
